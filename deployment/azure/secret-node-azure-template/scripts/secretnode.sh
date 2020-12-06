d180ef8f27ef7e570a9e5208d3f2ef0782326617
Duncan Ashe <d.ashe@cointel.ai>
2020-12-06 12:21:55 +0100

    remove sudo from chmod +x docker-compose

--- deployment/azure/secret-node-azure-template/scripts/secretnode.sh
+++ deployment/azure/secret-node-azure-template/scripts/secretnode.sh
@@ -39,7 +39,7 @@ echo "Installing docker-compose" >> /home/"$1"/install.progress.txt
 # systemctl status docker
 curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 echo "Creating secret node runner" >> /home/"$1"/install.progress.txt
 
