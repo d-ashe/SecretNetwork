6b13befd4bcacec0a9eaa88fbfa99607f3195a73
Duncan Ashe <d.ashe@cointel.ai>
2020-12-06 15:56:07 +0100

    change dockerce to dockerio, add sudo to curl dockercompose -o dir, reorder apt-get after install of transport-https

--- deployment/azure/secret-node-azure-template/scripts/secretnode.sh
+++ deployment/azure/secret-node-azure-template/scripts/secretnode.sh
@@ -22,12 +22,8 @@ sudo chmod -R -f 777 /tmp/aesmd || sudo chmod -R -f 777 /tmp/aesmd || true
 echo "Installing docker" >> /home/"$1"/install.progress.txt
 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce -y
+sudo apt install apt-transport-https -y
+sudo apt install ca-certificates curl software-properties-common docker.io -y
 
 echo "Adding user $1 to docker group" >> /home/"$1"/install.progress.txt
 sudo service docker start
@@ -37,7 +33,7 @@ sudo usermod -aG docker "$1"
 
 echo "Installing docker-compose" >> /home/"$1"/install.progress.txt
 # systemctl status docker
-curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
